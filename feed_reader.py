7a409c9ab6957330d19e3d4e1511b8ee205b1f33
Akhileshsakure <36232800+Akhileshsakure@users.noreply.github.com>
2022-03-29 11:24:10 +0530

    Added docstring and comments

--- feed_reader.py
+++ feed_reader.py
@@ -5,6 +5,8 @@ import requests
 import xml.etree.ElementTree as ET
 
 def feed_read(link):
+    '''feed_read() parses the response and prints the blogs from the RSS feed'''
+
     try:
         res = requests.get(link)
     except:
@@ -15,10 +17,20 @@ def feed_read(link):
         except ET.ParseError:
             print('An error occured while parsing ' + link +', kindly check your RSS url')
         else:
+            #All the items tags are stored in items
             items = root[0].findall('item')
-            print(root[0].find('title').text.upper())
+            N = 5
+            if not len(items)>=N:
+                N = len(items)
+
+            #prints the title of the RSS feed
+            print('~~~~~~~~~ '+root[0].find('title').text.upper()+' ~~~~~~~~~')
             print()
-            for item in items[:2]:
+
+            #iterates through items and prints title, description, link for each item
+            #Here only first five posts from each RSS feed are being printed for sake of simplicity
+            #and can be changed as per user's requirement by changing N
+            for item in items[:N]:
                 print('Title - ',item.find('title').text)
                 print('Description - ',item.find('description').text)
                 print('link - ',item.find('link').text)
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from feed_reader import feed_read
 
 if __name__ == '__main__':
+    '''Takes in user input and maps space separated the urls to list'''
     urls = list(map(str,input('Enter RSS URL(s):').split()))
+
     for url in urls:
+        '''Passes the RSS feed url as parameter in feed_read()'''
         feed_read(url)
